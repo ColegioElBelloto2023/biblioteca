1fda7d3d8950fe60c4377fdae2c157a752cc4905
fabyDiaz <fbl.diaz.n@gmail.com>
2025-10-21 23:27:12 -0300

    Version simplificada del proyecto

--- src/main/java/cl/colegioelbelloto/biblioteca/controller/LibroController.java
+++ src/main/java/cl/colegioelbelloto/biblioteca/controller/LibroController.java
@@ -16,30 +16,17 @@ public class LibroController {
 
     @Autowired
     LibroServiceImpl libroService;
-    @Autowired
-    GoogleBooksService googleBooksService;
+
 
     @GetMapping("/inicio")
     public String irInicio(){
         return "index";
     }
 
-    @GetMapping("/buscar")
-    public String buscarPorIsbn(@RequestParam String isbn, Model model) {
-        Libro libro = googleBooksService.buscarPorIsbn(isbn);
-        if (libro == null) {
-            model.addAttribute("error", "No se encontró información para el ISBN: " + isbn);
-            model.addAttribute("libro", new Libro());
-        } else {
-            model.addAttribute("libro", libro);
-        }
-        return "libro_form";
-    }
 
     @PostMapping("/guardar")
     public String guardarLibro(@ModelAttribute Libro libro, Model model) {
         try {
-            googleBooksService.buscarPorIsbn(libro.getIsbn());
             libroService.saveLibro(libro);
             model.addAttribute("mensaje", "Libro guardado exitosamente");
             model.addAttribute("libro", new Libro()); // Limpia el formulario
@@ -55,33 +42,5 @@ public class LibroController {
         model.addAttribute("libro", new Libro());
         return "libro_form";
     }
-
-    @GetMapping("/lista")
-    public String listarLibros(Model model) {
-        List<Libro> libros = libroService.AllLibros();
-        model.addAttribute("libros", libros);
-        return "lista_libros";
-    }
-
-    @GetMapping("/eliminar/{id}")
-    public String eliminarLibro(@PathVariable Long id, Model model) {
-        libroService.deleteLibro(id);
-        return "redirect:/libros/lista";
-    }
-
-    @GetMapping("/editar/{id}")
-    public String mostrarFormularioEditar(@PathVariable Long id, Model model) {
-        Libro libro = libroService.findByIdLibro(id);
-        model.addAttribute("libro", libro);
-        return "libro_form"; // reutiliza el mismo formulario
-    }
-
-    @PostMapping("/actualizar/{id}")
-    public String actualizarLibro(@PathVariable Long id, @ModelAttribute Libro libro) {
-        libroService.updateLibro(id, libro);
-        return "redirect:/libros/lista";
-    }
-
-
 
 }
